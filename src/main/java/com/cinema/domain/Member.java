4264f4b35b04849229b84fabcdd42663a0766db4
2425343005 <rlawldbs0628@naver.com>
2024-08-21 20:01:27 +0900

    -자리이동 등등 디자인 변경

--- src/main/java/com/cinema/domain/Member.java
+++ src/main/java/com/cinema/domain/Member.java
@@ -14,8 +14,8 @@ public class Member {
     @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long userID;
-    private String id;
-    private String password;
+    private String id; //로그인시 받는 id값
+    private String password; //로그인시 받는 password값
     private String name;
     private String email;
     private String phone;
